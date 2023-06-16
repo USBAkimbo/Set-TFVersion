66cdb5afd5be6eb7db9d2818c2aca92c62b0559e
USBAkimbo <71508071+USBAkimbo@users.noreply.github.com>
2023-06-16 23:23:59 +0100

    Update Set-TFVersion.psm1

--- Set-TFVersion.psm1
+++ Set-TFVersion.psm1
@@ -1,41 +1,39 @@
-# Requires Chocolately - The package manager for Windows
-# Run the below to install it in PowerShell as admin
-# https://chocolatey.org/install
-# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Terraform folder path
+$TFPath = "C:\ProgramData\Terraform"
 
-# Then install the latest version of Terraform (again as admin in PowerShell)
-# choco install terraform -y
-
-# And install the other versions (again as admin in PowerShell)
-# choco install terraform --version 1.2.9 --side-by-side -y
-# choco install terraform --version 1.1.9 --side-by-side -y
-# choco install terraform --version 1.0.11 --side-by-side -y
-# choco install terraform --version 0.15.5 --side-by-side -y
-# choco install terraform --version 0.14.11 --side-by-side -y
-# choco install terraform --version 0.13.7 --side-by-side -y
-# choco install terraform --version 0.12.31 --side-by-side -y
-# choco install terraform --version 0.11.15 --side-by-side -y
+# Ensure $TFPath exists
+if (!(Test-Path $TFPath)) {
+    New-Item -ItemType Directory -Path $TFPath
+}
 
 # Default to latest version
-Set-Alias -Scope "Global" tf "C:\ProgramData\chocolatey\lib\terraform\tools\terraform.exe"
+Set-Alias -Scope "Global" tf "$TFPath\terraform-1.5.0.exe"
 
 function Set-TFVersion {
     param (
         [Parameter(Mandatory=$true)]
-        [ValidateSet("Latest","1.2.9","1.1.9","1.0.11","0.15.5","0.14.11","0.13.7","0.12.31","0.11.15")]
+        [ValidateSet("1.5.0","1.4.6","1.3.9","1.2.9","1.1.9","1.0.11","0.15.5","0.14.11","0.13.7","0.12.31","0.11.15")]
         [String]
         $Version
     )
 
     switch ($Version) {
         $Version {
-            if ($Version -eq "Latest") {
-                Set-Alias -Scope "Global" tf "C:\ProgramData\chocolatey\lib\terraform\tools\terraform.exe"
-                Write-Host "Set tf to latest version"
+            # Check if we have $Version installed
+            if (Test-Path $TFPath\terraform-$Version.exe) {
+                # $Version is already installed so set alias
+                Set-Alias -Scope "Global" tf "$TFPath\terraform-$Version.exe"
             } else {
-                Set-Alias -Scope "Global" tf "C:\ProgramData\chocolatey\lib\terraform.$Version\tools\terraform.exe"
-                Write-Host "Set tf to version $Version"
+                # $Version is not installed so install it and set the alias
+                Write-Host "Terraform $Version is not installed, downloading..."
+                $URL = "https://releases.hashicorp.com/terraform/$Version/terraform_" + $Version + "_windows_amd64.zip"
+                Start-BitsTransfer $URL $TFPath\$Version.zip
+                Expand-Archive $TFPath\$Version.zip $TFPath
+                Move-Item $TFPath\terraform.exe $TFPath\terraform-$Version.exe
+                Remove-Item -Force $TFPath\$Version.zip
+                Set-Alias -Scope "Global" tf "$TFPath\terraform-$Version.exe"
             }
-		}
-	}
+            Write-Host "Set tf to version $Version"
+        }
+    }
 }
